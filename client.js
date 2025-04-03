7d28cccf476fc255ebb8dab01cc1285069af21c6
Poisonbeak <michele.alfano@davincimilazzo.edu.it>
2025-04-03 09:39:17 +0200

    N to N

--- client.js
+++ client.js
@@ -3,9 +3,19 @@ window.addEventListener("DOMContentLoaded", e => {
 
     NToNButton.addEventListener("click", e => {
         console.log(`Clicked on ${e.target.id}`);
+        fetch("http://localhost:5000/N:N")
+        .then(response => response.json())
+        .then(message => {
+            console.log(message);
+        })
     })
     DeleteNToN.addEventListener("click", e => {
         console.log(`Clicked on ${e.target.id}`);
+        fetch("http://localhost:5000/Delete/N:N")
+        .then(response => response.json())
+        .then(message => {
+            console.log(message);
+        })
     })
 
     OneToNButton.addEventListener("click", e => {
--- server.js
+++ server.js
@@ -10,6 +10,46 @@ let db = new sqlite3.Database(__dirname + "/mydb.db");
 app.use(express.static(__dirname));
 
 
+app.get("/N:N", (req, res) => {
+    db.serialize(() => {    // esegue le query in ordine
+        db.run(`CREATE TABLE "Contacts" (
+            "ContactID"	INTEGER NOT NULL,
+            "FirstName"	TEXT NOT NULL,
+            "LastName"	TEXT NOT NULL,
+            "Email"	TEXT NOT NULL UNIQUE,
+            "Phone"	TEXT UNIQUE,
+            PRIMARY KEY("ContactID" AUTOINCREMENT)
+        );`)
+        db.run(`CREATE TABLE "Groups" (
+            "GroupID"	INTEGER NOT NULL,
+            "Name"	TEXT NOT NULL,
+            PRIMARY KEY("GroupID" AUTOINCREMENT)
+        );`)
+        db.run(`CREATE TABLE "Contacts_Groups" (
+            "ContactID"	INTEGER NOT NULL,
+            "GroupID"	INTEGER NOT NULL,
+            PRIMARY KEY("ContactID","GroupID"),
+            FOREIGN KEY("ContactID") REFERENCES "Contacts"("ContactID"),
+            FOREIGN KEY("GroupID") REFERENCES "Groups"("GroupID")
+        );`)
+
+        db.close();
+        res.status(200).send(JSON.stringify("Tabelle create!"));
+    })
+})
+
+app.get("/Delete/N:N", (req, res) => {
+    db.serialize(() => {
+        db.run(`DROP TABLE Contacts`);
+        db.run(`DROP TABLE Groups`);
+        db.run(`DROP TABLE Contacts_Groups`);
+
+        db.close();
+        res.status(200).send(JSON.stringify("Tabelle eliminate!"));
+    })
+})
+
+
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 })
